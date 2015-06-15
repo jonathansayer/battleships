899a8db134c885128942257d9a08b8a6c03624e2
Natalie Akam <natalie.akam@gmail.com>
2015-06-15 12:05:11 +0100

    Added test and method to place ship on bpard

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 class Board
 
   def place_ship ship
+    ship.place
     "(1,1)"
   end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,8 +4,17 @@ describe Board do
   
   it { is_expected.to respond_to :place_ship }
 
-  it 'To return the location of the ship' do 
+  it 'places the ship on the board' do
     ship = double :ship
+    
+    expect(ship).to receive(:place)
+    
+    subject.place_ship ship
+  end
+
+  it 'returns the location of the ship' do 
+    ship = double :ship
+    allow(ship).to receive(:place) { true }
     expect(subject.place_ship ship).to eq "(1,1)"
   end 
 
